01743cda4743b19f1ed799b51a0091a22f482249
EricRamo <eric.ramos@estudintat.upc.edu>
2024-06-02 19:26:09 +0200

    mejora

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,24 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Link from "next/link";
+import { Archivo } from 'next/font/google'
+import { Eczar } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const archivo = Archivo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-archivo',
+})
+const eczar = Eczar({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-eczar',
+})
 
-export const metadata = {
-  title: "NextJS and React Demo",
-  description:
-    "A simple demo of the basic features of NextJS and React",
-};
-
-export default function RootLayout({ children }) {
+export default function Layout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header className="h-9 bg-blue-600 text-white flex flex-row items-center px-3 gap-4">
-          <Link href="/"><h1 className="m-0">Demo</h1></Link>
-          <Link href="/users">Users</Link>
-          <Link href="/products">Products</Link>
-          <div className="flex-1"></div>
-          <div>login</div>
-        </header>
+      <body className={archivo.variable + ' ' + eczar.variable}>
         {children}
-        <footer></footer>
       </body>
     </html>
-  );
+  )
 }
